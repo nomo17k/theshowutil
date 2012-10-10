b83185ccae1a0dcb6c382056b219536e6046ab15
Taro Sato <okomestudio@gmail.com>
2012-10-10 00:10:13 -0700

    now using setuptools

--- setup.py
+++ setup.py
@@ -2,47 +2,25 @@
 """
 MLB the Show Player Rating Utility
 """
-from distutils.core import setup
+import os
+from setuptools import setup
 
 
-__version__ = '0.1.0'
-__author__ = 'Taro Sato'
-__author_email__ = 'okomestudio@gmail.com'
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-def make_descriptions(docstr=__doc__):
-    """
-    Make __doc__ into short and long package descriptions.
-    """
-    docstrs = docstr.strip().split("\n")
-    description = docstrs[0].strip()
-    long_description = "\n".join(docstrs[2:])
-    return description, long_description
-
-
-def main():
-    descr_short, descr_long = make_descriptions()
-
-    scripts = ['bin/theshowrater',
-               'bin/theshowfinder']
-
-    setup(name='theshowutil',
-          version=__version__,
-          author=__author__,
-          author_email=__author_email__,
-          maintainer=__author__,
-          maintainer_email=__author_email__,
-          url='',
-          description=descr_short,
-          long_description=descr_long,
-          download_url='',
-          platforms=['Linux'],
-          license='GPL',
-          packages=['theshowutil'],
-          package_dir={'theshowutil': 'theshowutil'},
-          scripts=scripts,
-          ext_modules=[])
-
-
-if __name__ == "__main__":
-    main()
+setup(name='theshowutil',
+      version='0.1.0',
+      author='Taro Sato',
+      author_email='okomestudio@gmail.com',
+      url='https://github.com/nomo17k/theshowutil',
+      description='MLB the Show player rating utility',
+      long_description=read('README'),
+      license='BSD',
+      packages=['theshowutil'],
+      package_dir={'theshowutil': 'theshowutil'},
+      scripts=['bin/theshowrater',
+               'bin/theshowfinder'],
+      classifiers=["Development Status :: 3 - Alpha"]
+      )
