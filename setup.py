42c7b825aa3cecdcb5f7396d2df0c8acb802b37a
Taro Sato <okomestudio@gmail.com>
2012-10-10 00:50:51 -0700

    incremented ver. number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name='theshowutil',
-      version='0.1.0',
+      version='0.1.1',
       author='Taro Sato',
       author_email='okomestudio@gmail.com',
       url='https://github.com/nomo17k/theshowutil',
